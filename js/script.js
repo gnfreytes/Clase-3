83f67ff6dd2322e54e291467b39f2f666d00f3e7
gnfreytes <71354035+gnfreytes@users.noreply.github.com>
2020-10-18 11:57:39 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log("Bienvenidx a la Escuela");
+console.log("Espermos que encuentres");
+console.log("la información que necesites");
+
+var nombre = "colega";
+var apellido = "ENEBA LA PLATA";
+
+
+
+function alerta() {
+  alert ("Tu consulta fue enviada");
+
+}
